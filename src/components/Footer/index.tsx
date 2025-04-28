26a6ecdc5df3c49d476afebfec38b4233a85fced
Wang HoEun <hoeunwang@gmail.com>
2025-04-28 16:46:58 +0900

    style: Footer 시멘틱 태그로 변경

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,10 +2,10 @@ import React from "react"
 
 const Footer = () => {
     return(
-        <div>
+        <footer>
             Thank You for Visiting My Blog, Have a Good Day 😊
             <br/> @ 2025 Developer Ho Eun, Powered By Gatsby.
-        </div>
+        </footer>
     )
 }
 
