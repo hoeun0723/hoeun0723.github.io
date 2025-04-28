32f31b49a862c87a5e297edabd2d400c04f041df
Wang HoEun <hoeunwang@gmail.com>
2025-04-28 15:21:12 +0900

    chore: 초기 세팅 및 style component 세팅

---
+++ src/GlobalNavigation/BlogTitle.tsx
@@ -0,0 +1,14 @@
+import * as S from './GlobalNavigation.style'
+
+const PROFILE_IMAGE_LINK = 
+'image-removebg-preview (12)](https://github.com/user-attachments/assets/ab96ddb2-8b1c-4d75-9b78-5e1cc0b3cca2'
+
+const BlogTitle = () => {
+    return(
+        <S.BlogTitle>
+            <a href='/'>hoeun.devlog</a>
+        </S.BlogTitle>
+    )
+}
+
+export default BlogTitle
---
+++ src/Layout/index.tsx
@@ -0,0 +1,31 @@
+import Footer from '@/components/Footer'
+import GlobalNavigation from '@/GlobalNavigation'
+import Styles from '@/styles'
+import styled from '@emotion/styled'
+import {ReactNode} from 'react'
+
+
+interface LayoutProps {
+    children: ReactNode
+}
+
+const Layout = ({children}: LayoutProps) => {
+    return (
+        <Styles>
+            <Container>
+                <GlobalNavigation/>
+                <main>{children}</main>
+                <Footer/>
+            </Container>
+        </Styles>
+    )
+}
+
+export default Layout
+
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+`
