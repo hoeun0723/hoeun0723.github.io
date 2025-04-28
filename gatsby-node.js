1cd33b41bb5a4b1ab59eaf2f9b409c66469e0faf
Wang HoEun <hoeunwang@gmail.com>
2025-04-28 14:03:36 +0900

    chore: 필요한 라이브러리 설치 및 테스트 data 생성성

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,24 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
+const path = require('path')
+// exports.createPages = async ({ actions }) => {
+//   const { createPage } = actions
+//   createPage({
+//     path: "/using-dsg",
+//     component: require.resolve("./src/templates/using-dsg.js"),
+//     context: {},
+//     defer: true,
+//   })
+// }
 
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+// Setup Import Alias
+exports.onCreateWebpackConfig = ({getConfig,actions}) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        '@' : path.resolve(__dirname,'./src'),
+      },
+    },
   })
 }
