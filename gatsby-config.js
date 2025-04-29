2e84f94b8b37ecba6d4da07633db5ccff1157019
Wang HoEun <hoeunwang@gmail.com>
2025-04-29 11:03:28 +0900

    chore: github pages로 배포하기 위한 config 파일 설정 추후 ga 작업 및 seo 도 미리 설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,8 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
+const blogConfig = require('./blog-config')
 
-const { Placeholder } = require('gatsby-plugin-image');
-
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
+  pathPrefix: "/hoeunw.github.io",
+  siteMetadata: { ...blogConfig },
   plugins: [
     {
       resolve: `gatsby-plugin-typescript`,
@@ -23,8 +11,6 @@ module.exports = {
         allExtensions: true,
       },
     },
-    `gatsby-plugin-emotion`,
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -35,7 +21,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
+        name: `static`,
         path: `${__dirname}/static`,
       },
     },
@@ -49,8 +35,6 @@ module.exports = {
         }
       },
     },
-    `gatsby-plugin-image`,
-    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
@@ -59,12 +43,6 @@ module.exports = {
             resolve: 'gatsby-remark-smartypants',
             options: {
               dashes: 'oldschool',
-            },
-          },
-          {
-            resolve: 'gatsby-remark-prismjs',
-            options : {
-              classPrefix: 'language-',
             },
           },
           {
@@ -77,7 +55,6 @@ module.exports = {
           },
           {
             resolve: 'gatsby-remark-copy-linked-files',
-            options: {},
           },
           {
             resolve: 'gatsby-remark-external-links',
@@ -86,21 +63,74 @@ module.exports = {
               rel: 'nofollow',
             },
           },
+          {
+            resolve: `gatsby-remark-vscode`,
+            options: {
+              theme: {
+                default: 'Dark+ (default dark)',
+                parentSelector: {
+                  'body.dark': 'Dark+ (default dark)',
+                  'body.light': 'Light+ (default light)',
+                },
+              },
+            },
+          },
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              offsetY: `100`,
+              icon: `<svg aria-hidden="true" height="20" version="1.1" viewBox="0 0 16 16" width="20"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`,
+              // className: `custom-class`,
+              isIconAfterHeader: true,
+            },
+          },
+          `gatsby-remark-reading-time`,
+          'gatsby-remark-gifs',
         ],
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-vscode`,
-            options: {
-              theme: 'Dark (Visual Studio)', // Or install your favorite theme from GitHub
-            },
-          },
-        ],
+        siteUrl: blogConfig.siteUrl,
+        stripQueryString: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: blogConfig.title,
+        short_name: blogConfig.title,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: blogConfig.favicon,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: blogConfig.siteUrl,
+        sitemap: `${blogConfig.siteUrl}/sitemap.xml`,
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-google-gtag`,
+    //   options: {
+    //     // You can add multiple tracking ids and a pageview event will be fired for all of them.
+    //     trackingIds: [
+    //       blogConfig.gtag.ga, // 설정 Google Analytics / GA
+    //       // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+    //       // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+    //     ],
+    //   },
+    // },
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-sitemap',
   ],
 }
