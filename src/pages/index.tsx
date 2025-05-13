558352006c088de9b496a031351c16f212433c22
Wang HoEun <hoeunwang@gmail.com>
2025-05-13 13:59:50 +0900

    fix: index 페이지에서 categoryList -> MainHeader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { graphql } from "gatsby";
-import { IGatsbyImageData } from "gatsby-plugin-image";
+import { IGatsbyImageData } from "gatsby-plugin-image"
 import queryString, {ParsedQuery} from 'query-string'
 import { useMemo } from "react";
 
-import CategoryList, {CategoryListProps} from "@/components/CategoryList";
+import MainHeader from "@/components/MainHeader"
+import { CategoryListProps } from "@/components/CategoryList";
 import PostList, {PostType} from '@/components/PostList'
 import Layout from '@/Layout'
-import { PostListItemType } from "@/types/PostItem.types";
+import { PostListItemType } from "@/types/PostItem.types"
 
 type IndexPageProps = {
   location: {
@@ -69,7 +70,7 @@ const IndexPage = ({
    )
    return (
     <Layout>
-      <CategoryList selectedCategory={selectedCategory} categoryList={categoryList}/>
+      <MainHeader selectedCategory={selectedCategory} categoryList={categoryList}/>
       <PostList selectedCategory={selectedCategory} posts={edges} />
     </Layout>
    )
