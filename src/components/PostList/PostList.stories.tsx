797a0b5d4a83cc5a38ebd5cf74a6b4f920114ea8
Wang HoEun <hoeunwang@gmail.com>
2025-05-13 15:00:25 +0900

    story: postlist

---
+++ src/components/PostList/PostList.stories.tsx
@@ -0,0 +1,27 @@
+import PostList, { PostListProps } from './index'
+import { Meta, StoryObj } from '@storybook/react' // Storybook 8.x에서 최신 패키지
+
+import {posts} from '@/mocks/posts.mock'
+
+const meta: Meta<typeof PostList> = {
+  title: 'CATEGORY/PostList',
+  component: PostList,
+  args: {},
+}
+export default meta
+
+type Story = StoryObj<PostListProps>
+
+export const Default: Story = {
+  args: {
+    selectedCategory: 'All',
+    posts: posts,
+  },
+}
+
+export const SelectJSCategory: Story = {
+    args: {
+        selectedCategory: 'javascript',
+        posts: posts
+    }
+}
