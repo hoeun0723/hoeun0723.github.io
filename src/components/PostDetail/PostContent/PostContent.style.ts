fc81bfe2660a5babde781d44526d8630d2809b9b
Wang HoEun <hoeunwang@gmail.com>
2025-06-05 14:00:43 +0900

    feat: TOC 컴포넌트

--- src/components/PostDetail/PostContent/PostContent.style.ts
+++ src/components/PostDetail/PostContent/PostContent.style.ts
@@ -29,6 +29,17 @@ display: flex;
   * + h3 {
     margin-top: 32px;
   }
+
+   // TOC 클릭시 헤더가 보이게끔 이동하기 위한 속성
+  h1[id],
+  h2[id],
+  h3[id],
+  h4[id],
+  h5[id],
+  h6[id] {
+    scroll-margin-top: 6rem; // 각 태그의 높이 만큼
+  }
+
   hr + h1,
   hr + h2,
   hr + h3 {
