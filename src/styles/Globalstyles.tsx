54b24f967231c096da919abf53daa2afe149a7b0
Wang HoEun <hoeunwang@gmail.com>
2025-06-09 16:48:20 +0900

    design: --color-chip

--- src/styles/Globalstyles.tsx
+++ src/styles/Globalstyles.tsx
@@ -48,6 +48,7 @@ import fonts from '@/fonts'
      body {
     --color-background: #f3f5f7;
     --color-background-secondary: #ffffff;
+    --color-chip: #eceef1;
     --color-text: #00214d;
     --color-heading-text: #000;
     --color-paragraph: #1b2e46;
@@ -70,6 +71,7 @@ import fonts from '@/fonts'
   body.dark {
     --color-background: #242629;
     --color-background-secondary: #151519;
+    --color-chip: #242629;
     --color-text: #dbdbdb;
     --color-heading-text: #fff;
     --color-paragraph: #abb5c3;
