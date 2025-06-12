51da152ba141cb849f0b95c72ee00a71d4ce7d85
Wang HoEun <hoeunwang@gmail.com>
2025-06-12 14:40:29 +0900

    refactor: postContent 와 TOC 타입을 Merge 해서 PostBodyProps 타입 구성

--- src/components/PostDetail/PostBody/index.tsx
+++ src/components/PostDetail/PostBody/index.tsx
@@ -1,11 +1,7 @@
 import * as S from '../PostDetail.style'
-import PostContent from './PostContent'
-import TableOfContent from './TableOfContent'
-
-interface PostBodyProps {
-  tableOfContents: string
-  html: string
-}
+import PostContent, { PostContentProps } from './PostContent'
+import TableOfContent, { TableOfContentProps } from './TableOfContent'
+interface PostBodyProps extends PostContentProps, TableOfContentProps {}
 
 // 문자열 형태의 HTML 코드를 출력하는 것도 dangerouslySetInnerHTML 속성을 통해 간단하게 구현이 가능
 const PostBody = ({ tableOfContents, html }: PostBodyProps) => {
