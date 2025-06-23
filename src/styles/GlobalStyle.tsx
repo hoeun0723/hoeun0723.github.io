385631dcaf70f2438679e9c119f50c81ee660e18
Wang HoEun <hoeunwang@gmail.com>
2025-06-23 11:25:49 +0900

    refactor: 메인 색상 변동. gray 색감에서 latte 색감으로

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -55,47 +55,46 @@ const styles = css`
 
   body,
   body.light {
-    --color-background: #f3f5f7;
-    --color-background-secondary: #ffffff;
-    --color-category-chip: #eceef1;
-    --color-text: #00214d;
-    --color-heading-text: #000;
-    --color-paragraph: #1b2e46;
-    --color-anchor-text: #0076d1;
-    --color-code-background: #d7dad3;
-    --color-code-text: #212529;
-    --color-table-background-color: #fff;
-    --color-table-background-color-second: #f6f8fa;
-    --color-table-border: #d0d7de;
-    --color-navigation-shadow: rgba(0, 0, 0, 0.08);
+    --color-background: #ffffff;
+    --color-background-secondary: #fdf6ec;
+    --color-category-chip: #eceae6;
+    --color-text: #3a2e27;
+    --color-heading-text: #1e1e1e;
+    --color-paragraph: #5e5148;
+    --color-anchor-text: #c96f2b;
+    --color-code-background: #f0e6da;
+    --color-code-text: #3a2e27;
+    --color-table-background-color: #ffffff;
+    --color-table-background-color-second: #f7f3ef;
+    --color-table-border: #d8cbc1;
+    --color-navigation-shadow: rgba(0, 0, 0, 0.04);
+    --color-primary: #d78c49;
+    --color-secondary: #a8745d;
 
-    --color-primary: #218bff;
-    --color-secondary: #54aeff;
-    /* --color-tertiary: */
-    --color-white: #f3f5f7;
-    --color-black: #222;
-    --color-gray: #5a6072;
-    --color-gray-1000: #21232c;
-    --color-overlay: #61646bcc;
+    --color-white: #ffffff;
+    --color-black: #2e2e2e;
+    --color-gray: #7d7066;
+    --color-gray-1000: #4d433c;
+    --color-overlay: #5c5144cc;
   }
 
   body.dark {
-    --color-background: #242629;
-    --color-background-secondary: #151519;
-    --color-category-chip: #242629;
-    --color-text: #dbdbdb;
-    --color-heading-text: #fff;
-    --color-paragraph: #abb5c3;
-    --color-anchor-text: #0076d1;
-    --color-code-background: #495057;
-    --color-code-text: #f8f9fa;
-    --color-table-background-color: #151519;
-    --color-table-background-color-second: #0d1117;
-    --color-table-border: #424242;
+    --color-background: #1f1b18;
+    --color-background-secondary: #2a2522;
+    --color-category-chip: #3c3530;
+    --color-text: #e7e1dc;
+    --color-heading-text: #ffffff;
+    --color-paragraph: #d2c4bd;
+    --color-anchor-text: #ffcc8f;
+    --color-code-background: #3e372f;
+    --color-code-text: #ffe9d6;
+    --color-table-background-color: #2a2522;
+    --color-table-background-color-second: #1a1816;
+    --color-table-border: #6f5f55;
     --color-navigation-shadow: rgba(0, 0, 0, 0.08);
 
-    --color-primary: #79c0ff;
-    --color-secondary: #1f6feb;
+    --color-primary: #f5b56b;
+    --color-secondary: #d1a175;
     /* --color-tertiary:  */
   }
 
